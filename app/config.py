73b51ecaceace35e8aeea3b18d1d064038ef1bd8
kesavapriya <dkesavapriya@gmail.com>
2025-02-11 09:47:04 +0530

    changed

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")  # Change this to a strong secret key
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:MSD@localhost/zerohunger")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://username:password@localhost/databasname")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwtsecretkey")  # Change this for security
     SOCKETIO_MESSAGE_QUEUE = os.getenv("REDIS_URL", "redis://")  # Required for real-time notifications
